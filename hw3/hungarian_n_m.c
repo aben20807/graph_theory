7709e9bdb55c338ab2a374da32b8a0ab8503cf95
aben20807 <aben20807@gmail.com>
2018-12-08 19:40:19 +0800

    Use long long type to pass prob9, 10

--- hw3/hungarian_n_m.c
+++ hw3/hungarian_n_m.c
@@ -3,24 +3,21 @@
 #include <stdbool.h>
 #include <limits.h>
 
-int **create_G(const int n);
-void print_G(int **g, const int n);
-void destroy_G(int **g, const int n);
-int **compute_new_w(const int n, int **w, const int *u, const int *v);
-int **get_equality_subgraph(int **w, const int n);
-// int **get_adj(int *set_X, const int n);
-int **get_adj(int **g, const int n);
-bool dfs_bm(int **guv, const int i, const int m, bool *vis, int *set_T);
-int *max_bipartite_matching(int **guv, const int n);
-// bool dfs_vc(int **g, const int i, const int m, const bool is_X, bool *vis_X, bool *vis_Y);
-// bool dfs_vc(int **g, const int i, const int m, const bool is_X, bool *vis_X, bool *vis_Y, int *set_Y);
-// void dfs_vc(int **M, const int i, const int n, int *vis);
-// void dfs_vc(int **g, const int i, const int n, int *vis, int *set_Y);
-void dfs_vc(int **g, const int i, const int n, int *mark_X, int *mark_Y, int *set_Y);
-int *min_vertex_cover(int **g, int *set_Y, const int n);
+typedef long long i64;
+
+i64 **create_G(const int n);
+void print_G(i64 **g, const int n);
+void destroy_G(i64 **g, const int n);
+i64 **compute_new_w(const int n, i64 **w, const i64 *u, const i64 *v);
+i64 **get_equality_subgraph(i64 **w, const int n);
+int **get_adj(i64 **g, const int n);
+bool dfs_bm(i64 **guv, const int i, const int m, bool *vis, int *set_T);
+int *max_bipartite_matching(i64 **guv, const int n);
+void dfs_vc(i64 **g, const int i, const int n, int *mark_X, int *mark_Y, int *set_Y);
+int *min_vertex_cover(i64 **g, int *set_Y, const int n);
 bool is_perfect_matching(const int *set_T, const int n, const int matching_size);
-int compute_epsilon(int **w, const int n, const int *min_vc);
-void adjust_u_v(int *u, int *v, const int n, const int *min_vc, const int epsilon);
+i64 compute_epsilon(i64 **w, const int n, const int *min_vc);
+void adjust_u_v(i64 *u, i64 *v, const int n, const int *min_vc, const i64 epsilon);
 
 int main()
 {
@@ -29,14 +26,14 @@ int main()
     scanf("%d", &m);
     int sq_size = (n > m ? n : m);
     int matching_size = (n < m ? n : m) + 1;
-    int *u = calloc(sq_size, sizeof(int));
+    i64 *u = calloc(sq_size, sizeof(i64));
     // XXX min
-    int *min = calloc(sq_size, sizeof(int));
-    int *v = calloc(sq_size, sizeof(int));
-    int **w = create_G(sq_size);
+    // int *min = calloc(sq_size, sizeof(int));
+    i64 *v = calloc(sq_size, sizeof(i64));
+    i64 **w = create_G(sq_size);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            scanf("%d", &w[i][j]);
+            scanf("%lld", &w[i][j]);
             // u_i = max_j w_ij
         }
     }
@@ -45,10 +42,10 @@ int main()
         for (int j = 0; j < sq_size; j++) {
             if (!j) {
                 u[i] = w[i][j];
-                min[i] = w[i][j];
+                // min[i] = w[i][j];
             } else {
                 u[i] = u[i] > w[i][j] ? u[i] : w[i][j];
-                min[i] = min[i] < w[i][j] ? min[i] : w[i][j];
+                // min[i] = min[i] < w[i][j] ? min[i] : w[i][j];
             }
             // w[i][j] += (w[i][j] == INT_MIN ? min[i] : 0);
             // w[i][j] = (w[i][j] == INT_MIN ? 0 : w[i][j]);
@@ -61,10 +58,11 @@ int main()
     for (int i = 0; i < sq_size; i++) {
         ans_T[i] = -1;
     }
+    /* Hungarian algorithm */
     do {
-        int **new_w = compute_new_w(sq_size, w, u, v);
+        i64 **new_w = compute_new_w(sq_size, w, u, v);
         // print_G(new_w, sq_size);
-        int **Guv = get_equality_subgraph(new_w, sq_size);
+        i64 **Guv = get_equality_subgraph(new_w, sq_size);
         // print_G(Guv, sq_size);
         int *set_T = max_bipartite_matching(Guv, sq_size);
         // for (int i = 0; i < sq_size; i++) {
@@ -79,7 +77,7 @@ int main()
             }
         }
 
-        int epsilon = compute_epsilon(new_w, sq_size, min_vc);
+        i64 epsilon = compute_epsilon(new_w, sq_size, min_vc);
         // for (int i = 0; i < sq_size; i++) {
         //     printf("%d ", u[i]);
         // }
@@ -104,13 +102,13 @@ int main()
     } while (!is_perfect);
 
 
-    int max_w = 0;
+    i64 max_w = 0;
     for (int i = 0; i < sq_size; i++) {
         if (ans_T[i] != -1 && w[ans_T[i]][i] >= 0) {
             max_w += w[ans_T[i]][i];
         }
     }
-    printf("%d", max_w);
+    printf("%lld", max_w);
 
     destroy_G(w, sq_size);
     free(ans_T);
@@ -119,34 +117,41 @@ int main()
     return 0;
 }
 
-int **create_G(const int n)
+/*
+ * Function: create_G
+ *  Malloc a n by n i64 type matrix (2D array) and initialize all elements
+ *  to 0 (by calloc).
+ *
+ * n: the number of matrix
+ */
+i64 **create_G(const int n)
 {
-    int **ret = (int **)calloc(n, sizeof(int *));
+    i64 **ret = (i64 **)calloc(n, sizeof(i64 *));
     for (int i = 0; i < n; i++) {
-        ret[i] = calloc(n, sizeof(int));
+        ret[i] = calloc(n, sizeof(i64));
     }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            // ret[i][j] = INT_MIN;
-            ret[i][j] = 0;
-        }
-    }
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < n; j++) {
+    //         // ret[i][j] = INT_MIN;
+    //         ret[i][j] = 0;
+    //     }
+    // }
     return ret;
 }
 
-void print_G(int **g, const int n)
+void print_G(i64 **g, const int n)
 {
     printf("\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            printf("%d ", g[i][j]);
+            printf("%lld ", g[i][j]);
         }
         printf("\n");
     }
     printf("\n");
 }
 
-void destroy_G(int **g, const int n)
+void destroy_G(i64 **g, const int n)
 {
     for (int i = 0; i < n; i++) {
         free(g[i]);
@@ -154,9 +159,9 @@ void destroy_G(int **g, const int n)
     free(g);
 }
 
-int **compute_new_w(const int n, int **w, const int *u, const int *v)
+i64 **compute_new_w(const int n, i64 **w, const i64 *u, const i64 *v)
 {
-    int **ret = create_G(n);
+    i64 **ret = create_G(n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             ret[i][j] = u[i] + v[j] - w[i][j];
@@ -165,9 +170,9 @@ int **compute_new_w(const int n, int **w, const int *u, const int *v)
     return ret;
 }
 
-int **get_equality_subgraph(int **w, const int n)
+i64 **get_equality_subgraph(i64 **w, const int n)
 {
-    int **ret = create_G(n);
+    i64 **ret = create_G(n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             ret[i][j] = w[i][j] == 0 ? 1 : 0;
@@ -176,22 +181,22 @@ int **get_equality_subgraph(int **w, const int n)
     return ret;
 }
 
-int **get_adj(int **g, const int n)
-{
-    int **ret = create_G(2 * n);
-    for (int i = 0; i < 2 * n; i++) {
-        for (int j = 0; j < 2 * n; j++) {
-            ret[i][j] = 0;
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            ret[i][j + n] = (g[i][j] == 1 ? 1 : 0);
-            ret[j + n][i] = (g[i][j] == 1 ? 1 : 0);
-        }
-    }
-    return ret;
-}
+// int **get_adj(i64 **g, const int n)
+// {
+//     int **ret = create_G(2 * n);
+//     for (int i = 0; i < 2 * n; i++) {
+//         for (int j = 0; j < 2 * n; j++) {
+//             ret[i][j] = 0;
+//         }
+//     }
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < n; j++) {
+//             ret[i][j + n] = (g[i][j] == 1 ? 1 : 0);
+//             ret[j + n][i] = (g[i][j] == 1 ? 1 : 0);
+//         }
+//     }
+//     return ret;
+// }
 
 // get_matching
 // int **get_adj(int *set_Y, const int n)
@@ -210,7 +215,7 @@ int **get_adj(int **g, const int n)
 //     return ret;
 // }
 
-bool dfs_bm(int **guv, const int i, const int m, bool *vis, int *set_Y)
+bool dfs_bm(i64 **guv, const int i, const int m, bool *vis, int *set_Y)
 {
     for (int j = 0; j < m; j++) {
         if (guv[i][j] && !vis[j]) {
@@ -224,7 +229,7 @@ bool dfs_bm(int **guv, const int i, const int m, bool *vis, int *set_Y)
     return false;
 }
 
-int *max_bipartite_matching(int **guv, const int n)
+int *max_bipartite_matching(i64 **guv, const int n)
 {
     int *set_Y = calloc(n, sizeof(int));
     for (int i = 0; i < n; i++) {
@@ -243,7 +248,7 @@ int *max_bipartite_matching(int **guv, const int n)
     return set_Y;
 }
 
-void dfs_vc(int **g, const int i, const int n, int *mark_X, int *mark_Y, int *set_Y)
+void dfs_vc(i64 **g, const int i, const int n, int *mark_X, int *mark_Y, int *set_Y)
 {
     if (mark_X[i]) {
        return;
@@ -259,15 +264,17 @@ void dfs_vc(int **g, const int i, const int n, int *mark_X, int *mark_Y, int *se
     }
 }
 
-int *min_vertex_cover(int **g, int *set_Y, const int n)
+int *min_vertex_cover(i64 **g, int *set_Y, const int n)
 {
     // [X(R) | Y(T)]
     int *mvc = calloc(n * 2, sizeof(int));
-    int *mark_X = calloc(n, sizeof(int));
-    int *mark_Y = calloc(n, sizeof(int));
+    int mark_X[n];
+    int mark_Y[n];
     int set_X[n];
     for (int i = 0; i < n; i++) {
         set_X[i] = -1;
+        mark_X[i] = 0;
+        mark_Y[i] = 0;
     }
     for (int i = 0; i < n; i++) {
         int x_matched_idx = set_Y[i];
@@ -284,8 +291,6 @@ int *min_vertex_cover(int **g, int *set_Y, const int n)
     }
     for (int i = 0; i < n; i++) {
         mvc[i] = !mark_X[i];
-    }
-    for (int i = 0; i < n; i++) {
         mvc[i + n] = mark_Y[i];
     }
     for (int i = 0; i < 2 * n; i++) {
@@ -310,7 +315,7 @@ bool is_perfect_matching(const int *set_T, const int n, const int matching_size)
     return matching == n;
 }
 
-int compute_epsilon(int **w, const int n, const int *min_vc)
+i64 compute_epsilon(i64 **w, const int n, const int *min_vc)
 {
     int epsilon = INT_MAX;
     for (int i = 0; i < n; i++) {
@@ -328,7 +333,7 @@ int compute_epsilon(int **w, const int n, const int *min_vc)
     return epsilon;
 }
 
-void adjust_u_v(int *u, int *v, const int n, const int *min_vc, const int epsilon)
+void adjust_u_v(i64 *u, i64 *v, const int n, const int *min_vc, const i64 epsilon)
 {
     for (int i = 0; i < n; i++) {
         if (!min_vc[i]) {
